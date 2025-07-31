4374801f8614071b366d9451cb06e37dcacd5af7
VeenaVinayan <veenavinayan@gmail.com>
2025-07-31 13:35:07 +0530

    final commit

--- difference.js
+++ difference.js
@@ -6,3 +6,6 @@ console.log("Difference is ",difference(89,19));
 console.log("Difference is ",difference(839,99));
 console.log("Difference is ",difference(39,9));
 console.log("Difference is ",difference(900,9));
+console.log("Difference is ",difference(500,9));
+console.log("Difference is ",difference(9,9));
+console.log("Difference is ",difference(90,9));
--- sum.js
+++ sum.js
@@ -1,7 +1,16 @@
 const sum = (a,b)=>{
      return a+b;
 }
+function sumList(...args){
+    return args.reduce((acc,value)=> acc+value,0)
+}
 
 console.log('Result ::',sum(10,5));
 console.log('Result ::',sum(20,12));
 console.log("Result ::",sum(35,5));
+console.log("Sum Array is ::",sumList(12,8,10,15,20,50,75,100));
+
+const a =[1,100,30,2,11,3,25,324,9,10,5];
+const b = [...a]; // shallow copy --- another reference
+a.sort((a,b)=>a-b);
+console.log(`List 1 :: ${a} ---> List  :::${b} `);
