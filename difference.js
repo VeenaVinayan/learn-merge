a71e1118473edd14195c8cc08e1c1df58dff2dee
VeenaVinayan <veenavinayan@gmail.com>
2025-07-31 13:20:36 +0530

    Initial commit to Difference

---
+++ difference.js
@@ -0,0 +1,8 @@
+const difference  = (a,b)=>{
+     return a-b;
+}
+
+console.log("Difference is ",difference(89,19));
+console.log("Difference is ",difference(839,99));
+console.log("Difference is ",difference(39,9));
+console.log("Difference is ",difference(900,9));
